0bd3ce1a5902cda618f67f143daf22492540c863
mehrab-khan <mehrab.hossain282005@gmail.com>
2026-04-04 15:01:32 +0600

    fixed

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 import express from 'express'
+import productRoutes from './routes/productRoutes.js'
 import connectDB from './db/connectDB.js'
 import cors from 'cors'
 import orderRoutes from './routes/orderRoutes.js'
+import dotenv from 'dotenv'
+dotenv.config() // my this thing loads my env file
 
 const app = express()
-const port = 3000
-const database_url = 'mongodb://localhost:27017/'
+const port = process.env.PORT
+const database_url = process.env.MONGO_URI
 
 //connect database
     connectDB(database_url)
@@ -15,7 +18,8 @@ const database_url = 'mongodb://localhost:27017/'
     app.use(express.json())
 //routes
     app.use('/api/order', orderRoutes)
+    app.use('/api/product', productRoutes)
 
 app.listen(port, ()=>{
-    console.log("Server is listening at port ",port)
+    console.log("Server is listening...")
 })
---
+++ routes/productRoutes.js
@@ -0,0 +1,12 @@
+import {getData, createNewData, updateData, readData, deleteData} from '../controllers/productController.js'
+import express from 'express'
+
+const route = express.Router()
+
+route.get('/', getData)
+route.post('/new', createNewData)
+route.delete('/delete/:id',deleteData)
+route.put('/update/:id', updateData)
+route.get('/read/:id', readData)
+
+export default route
